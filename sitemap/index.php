d61497314a847332bb2b89bfe7a0c5d6ef67bc84
luizj <lngj2003@gmail.com>
2015-06-05 12:40:54 -0300

    Update index.php

--- sitemap/index.php
+++ sitemap/index.php
@@ -3,109 +3,96 @@ $url = $_SERVER["SERVER_PORT"]==443?"https":"http";;
 $url .= "://".$_SERVER["HTTP_HOST"];
 ?>
 <!DOCTYPE HTML>
-<html lang="pt-br">
+<html lang="en">
 <head>
 <title>Preula Sitemap Generator</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <script src="jquery-2.1.4.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 
-var paginas=[["/",false]];
-var processos=[];
-var max_processos=100;
-var tempo = 0;
-var ultima_lida = 0;
+var pages=[["/",false,[]]]; //[url,read,array_images]
+var images=['/']; //[url]
+var process=[];
+var max_process=50;
+var timer = 0;
+var last_read = 0;
+var paused=false;
 
-for(var i=0; i<max_processos; i++)processos[i] = false;
-
-function IsJsonString(str){
-	if(typeof $.parseJSON(str) =='object'){
-		return true;
-	}else{
-		return false;
-    }
-    return true;
-}
+for(var i=0; i<max_process; i++)process[i] = false;
 
 function get_url(){
-	var id_processo=9999;
-	for(var i=0; i<=max_processos; i++)
-    {
-        if(processos[i] == false){
-        	processos[i] = true;
-        	id_processo=i;
-        	break;
-        }
-    }
-	if(id_processo==9999)return;
+	$("#images").html(images.length);
+	$("#total").html(pages.length);
+    $("#reads").html((last_read+1));
+
+    var tmp_proc = $.grep(process, function(a){return a===true;}).length;
+    $("#process").html(tmp_proc);
+
+	if(paused)return;
 
 	var url="";
-	for(var j=ultima_lida; j<$(paginas).length; j++)
+	var j=last_read;
+	for(; j<$(pages).length; j++)
     {
-    	ultima_lida=j;
-        if(paginas[j][1] == false){
-        	url= paginas[j][0];
-        	paginas[j][1] = true;
+    	last_read=j;
+        if(pages[j][1] == false){
+        	url = pages[j][0];
+        	pages[j][1] = true;
         	break;
         }
     }
-    if(url=="")return;
-    $("#info").html(url);
+	if(url==""){
+		if(tmp_proc==0 && pages.length==(last_read+1)){
+			pause();
+			$('#btn_start').prop('disabled', true);
+			gerar_xml();
+		}
+		return;
+	}
+	var id_process = process.indexOf(false);
+	if(id_process >= 0){
+		process[id_process] = true;
+    }else{return;}
 
+    $("#info").html(url);
     $.ajax({
-		url: "scan.php?url="+url,
+		url: "scan.php",
 		dataType: "json",
 		async: true,
+		type: "POST",
+		data: {d:url},
+		timeout: 1000*600,
 		success: function(data){
-			var temp=[]; //[url,lida]
-			var found=false;
+			process[id_process] = false;
 
+			var temp=[]; //[url,read,array_images]
 			$.each(data, function(key,val){
-				if(val[0]!="/")return $("#error").append(url+" ("+val+")<br>");
+				if(val.f=="1"){
+					if($.inArray(val.u, images) >= 0){
+						return;
+					}
+				    images.push(val.u);
+					temp = [val.u, val.t, val.m];
+					pages[j][2].push(temp);
+					return;
+				}
+				if(val=="/")return;
+				if(val.u == undefined || val.u[0]!="/"){
+					console.log(val);
+					$("#error").append(url+" ("+val.u+")<br>");
+					return;
+				}
+				if(val.u=="/")return;
 
-				found=false;
-		        for(var k=0; k<paginas.length; k++)
-		        {
-		            if(paginas[k][0] == val){
-		            	found=true;
-		            	break;
-		            }
-		        }
-				if(!found){
-					temp=[val,false];
-					paginas.push(temp);
+				if($.grep(pages, function(a){return a[0]===val.u;}).length === 0){
+					temp = [val.u, false, []];
+					pages.push(temp);
 				}
 			});
-
-			$("#total").html(paginas.length);
-	        $("#lidas").html((ultima_lida+1));
-
-	        processos[id_processo] = false;
-	        var tmp_proc=0;
-	        for(var m=0; m<=max_processos; m++)
-	        {
-	            if(processos[m]==true)tmp_proc++;
-	        }
-	        $("#processos").html(tmp_proc);
-
-	        /*if(paginas.length>1 && paginas.length==(ultima_lida+1) && tmp_proc==(max_processos-1)){
-	        	$("#info").html("Finish");
-				$('#btn_start').prop('disabled', false);
-				$('#btn_pause').prop('disabled', true);
-				$('#btn_gerar').prop('disabled', false);
-				window.clearInterval(tempo);
-	        }*/
 		},
 		error: function(d,e){
-			$("#info").html('['+e+'] Error URL: '+url);
-
-			processos[id_processo] = false;
-	        var tmp_proc=0;
-	        for(var m=0; m<=max_processos; m++)
-	        {
-	            if(processos[m]==true)tmp_proc++;
-	        }
-	        $("#processos").html(tmp_proc);
+			console.error('['+e+'] Error URL: '+url);
+			process[id_process] = false;
 		}
 	});
 }
@@ -113,34 +100,36 @@ function get_url(){
 function start(){
 	$('#btn_start').prop('disabled', true);
 	$('#btn_pause').prop('disabled', false);
-	$('#btn_gerar').prop('disabled', true);
-	tempo = window.setInterval('get_url()', 10);
+	$('#btn_create').prop('disabled', true);
+	paused = false;
+	if(timer)window.clearInterval(timer);
+	timer = window.setInterval('get_url()', 30);
 }
 
 function pause(){
 	$("#info").html("Paused");
 	$('#btn_start').prop('disabled', false);
 	$('#btn_pause').prop('disabled', true);
-	$('#btn_gerar').prop('disabled', false);
-	window.clearInterval(tempo);
+	$('#btn_create').prop('disabled', false);
+	paused = true;
 }
 
-function gerar_xml(){
+function create_xml(){
 	$("#info").html('Wait, creating the sitemap.');
-	$('#btn_gerar').prop('disabled', true);
+	$('#btn_create').prop('disabled', true);
 	$.ajax({
 		type: "POST",
 		url: "save.php",
-		data: {data:JSON.stringify(paginas)},
+		data: {data:JSON.stringify(pages)},
 		async: true,
 		contentType: "application/x-www-form-urlencoded;charset=UTF-8",
 		success: function(d){
 			$("#info").html(d);
-			$('#btn_gerar').prop('disabled', false);
+			$('#btn_create').prop('disabled', false);
 		},
 		error: function(d,e){
 			$("#info").html('['+e+'] XML Error');
-			$('#btn_gerar').prop('disabled', false);
+			$('#btn_create').prop('disabled', false);
 		}
 	});
 }
@@ -150,11 +139,12 @@ function gerar_xml(){
 <body>
 <h3>Creating Sitemap: <?php echo $url;?></h3>
 Total Pages: <label id="total">0</label><br>
-Reading Page: <label id="lidas">0</label><br>
-Process: <label id="processos">0</label><br><br>
+Total Images: <label id="images">0</label><br>
+Reading Page: <label id="reads">0</label><br>
+Process: <label id="process">0</label><br><br>
 <button onclick="start();" id="btn_start">Start</button>
 <button onclick="pause();" id="btn_pause" disabled>Pause</button>
-<button onclick="gerar_xml();" id="btn_gerar" disabled>Create XML</button>
+<button onclick="create_xml();" id="btn_create" disabled>Create XML</button>
 <br><br>
 Info: <label id="info"></label><br>
 <div style="color:red;width:100%;" id="error">Errors:<br></div>
