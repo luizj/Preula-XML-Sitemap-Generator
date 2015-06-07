0f0ed80f0ebad68c117fe5558f7cc0271d231416
luizj <lngj2003@gmail.com>
2015-06-06 21:30:00 -0300

    Update save.php

--- sitemap/save.php
+++ sitemap/save.php
@@ -26,7 +26,7 @@ function AddHeader($sitemap){
 
 function AddHeader_image($sitemap){
 	AddXML($sitemap,
-			'<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">',
+			'<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">',
 			"w+");
 }
 
